33d0c50c589c5fadbef24ea60fbeeb1abe298d65
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-11-28 14:39:15 +0300

    if/else loops

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,6 +4,6 @@ for i in range(100):
     if i < 10:
         print("0{:d}, ".format(i), end="")
     elif i == 99:
-        print("{:d} ".format(i), end="")
+        print(f"{i}")
     else:
         print("{:d}, ".format(i), end="")
