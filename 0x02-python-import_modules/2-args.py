9cfe50b13f9fc07b5e9181d5250dba27e2bc2944
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-11-30 15:25:27 +0300

    Python Modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
 
     length = len(sys.argv) -1
 
-    if length > 1:
-        print("{:d} arguments".format(length))
-    else:
+    if length == 1:
         print("{:d} argument".format(length))
+    else:
+        print("{:d} arguments".format(length))
     for i in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(i, sys.argv[i]))
