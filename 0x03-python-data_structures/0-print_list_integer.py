02467d72cad23d9898c570891de4c241dfcfda91
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-12-02 10:21:08 +0300

    lists/tuples

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    list(map(lambda x: print(my_list[x]), range(len(my_list))))
+    list(map(lambda x: print("{:d}".format(my_list[x])), range(len(my_list))))
     # for i in range(len(my_list)):
     #     print(my_list[i])
