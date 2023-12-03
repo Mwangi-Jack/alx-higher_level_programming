e789f957b199c66eaec84ccdf34239e83ee1be65
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-12-03 13:32:15 +0300

    lists/tuples

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def  divisible_by_2(my_list=[]):
+    new_list = []
+
+    for index, value in enumerate(my_list):
+        if value % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+
+    return new_list
