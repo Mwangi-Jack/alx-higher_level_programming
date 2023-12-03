161e117a5c5f2a19ba67c9e21c62862410b02135
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-12-03 12:59:49 +0300

    lists/tuples

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    if len(my_list) > 0:
-        max_val = 0
 
-        for index, value in enumerate(my_list):
-            if value > max_val:
-                max_val = value
-
-        return max_val
-
-    else:
+    if not my_list:
         return None
+
+    max_val = 0
+
+    for index, value in enumerate(my_list):
+        if value > max_val:
+            max_val = value
+
+    return max_val
+
