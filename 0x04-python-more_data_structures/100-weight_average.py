0808e52bd97a5d394b026939ca8d69bff31ea3c6
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-12-06 18:03:22 +0300

    more data structures

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    summation = 0
+    divident = 0
+    if not my_list:
+        return 0
+    for i in range(len(my_list)):
+        mul = 1
+        divident += my_list[i][len(my_list[i])-1]
+        for j in my_list[i]:
+            mul = mul * j
+
+        summation = summation + mul
+
+    return float(summation/divident)
