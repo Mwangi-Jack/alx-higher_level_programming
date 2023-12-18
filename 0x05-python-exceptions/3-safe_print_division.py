5f94487d194d74a30aebbbb2715b578067ec4bc9
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-12-18 18:03:34 +0300

    safe perform division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    """Safe print division.
-    Args:
-        a (int): value to divide with
-        b (int): value to divide with
-    Returns:
-        returns the quotient.
-    """
 
     try:
         quotient = a / b
