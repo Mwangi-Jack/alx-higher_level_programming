7af3a0f9af7ae06fb0252545c2a855427b0728d3
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-12-18 17:59:45 +0300

    safe perform division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """Safe print division.
+    Args:
+        a (int): value to divide with
+        b (int): value to divide with
+    Returns:
+        returns the quotient.
+    """
+
+    try:
+        quotient = a / b
+    except (ZeroDivisionError, TypeError):
+        quotient = None
+    finally:
+        print("Inside result: {}".format(quotient))
+    return (quotient)
