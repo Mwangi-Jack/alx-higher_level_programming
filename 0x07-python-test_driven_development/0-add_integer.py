e1e936a120ed424ac1c29e8e95bb55ec16f8ffd1
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-01-10 14:25:56 +0300

    add integer test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,14 +10,14 @@ def add_integer(a, b=98):
     Returns the sum of a and b
 
     Arguments:
-    	a: first number
-	b: second number(Default value of 98)
+        a: first number
+    b: second number(Default value of 98)
 
     Raises:
-    	TypeError: When Operands given are not valid -> Integers
+        TypeError: When Operands given are not valid -> Integers
 
     Returns:
-    	int: Sum of a and b
+        int: Sum of a and b
     """
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
@@ -26,9 +26,9 @@ def add_integer(a, b=98):
         raise TypeError('b must be an integer')
 
     try:
-       int(a)
-       int(b)
+        int(a)
+        int(b)
     except Exception as e:
-       raise e
+        raise e
 
     return int(a) + int(b)
