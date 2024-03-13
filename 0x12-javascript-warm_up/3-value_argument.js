fcd18c40e6c45ce8e005ee9c2fff8b6815d067a7
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-03-13 11:49:36 +0300

    value argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-
-if (args.length === 1) {
+if (!process.argv[2]) {
   console.log('No argument');
 } else {
-  console.log(args[0]);
+  console.log(process.argv[2]);
 }
