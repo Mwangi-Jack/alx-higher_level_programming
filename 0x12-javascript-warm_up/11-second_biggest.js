bfafc6832fb7a4ac00cc2451c0a04fe256f1dbbf
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-03-13 15:45:33 +0300

    creating module exports

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log(0);
+} else {
+  if (args.length === 1) {
+    console.log(0);
+  } else {
+    const order = args.sort((a, b) => a - b);
+    console.log(order);
+    console.log(order[args.length - 2]);
+  }
+}
