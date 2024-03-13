60cae4930e29b0904dca4f31a0eb5a888c38e195
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-03-13 11:45:18 +0300

    factorial using recursion

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,10 +5,10 @@ const args = process.argv.slice(2);
 const num = parseInt(args[0]);
 
 function fact (a) {
-	if (isNaN(a) || a === 1) {
-		return 1;
-	}
-	return a * fact(a - 1);
+  if (isNaN(a) || a === 1) {
+    return 1;
+  }
+  return a * fact(a - 1);
 }
 
 console.log(fact(num));
