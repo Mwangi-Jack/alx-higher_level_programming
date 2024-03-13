460fcb5267196b4220994627a6062719fdcc0898
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-03-13 22:40:55 +0300

    creating a rectangle class with constructor

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,8 +9,8 @@ class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.width; i++) {
-      let s = '';
+    for (let i = 0, s; i < this.width; i++) {
+      s = '';
       for (let j = 0; j < this.height; j++) {
         s += 'X';
       }
