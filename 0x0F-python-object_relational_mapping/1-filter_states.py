5aceb61ebc1547e775394aac8b9f683a543dfef4
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-03-17 15:04:08 +0300

    filtering by user input

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    args = sys.argv
+
+    db = MySQLdb.connect(host=args[1], password=args[2], database=args[3])
+
+    conn = db.cursor()
+
+    conn.execute("""SELECT * FROM states WHERE name = 'New York'""")
+
+    states = conn.fetchall()
+
+    for state in states:
+        print(state)
