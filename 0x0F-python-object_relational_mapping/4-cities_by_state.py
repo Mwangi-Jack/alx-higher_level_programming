862c800113da54406b00c13fd01266829bd561eb
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-03-17 17:23:36 +0300

    fetching cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Script to list all 'cities' from the database
+'hbtn_0e_4_usa
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2], database=args[3])
+
+    conn = db.cursor()
+
+    conn.execute("SELECT id, name, (SELECT name FROM states WHERE id = state_id) from cities")
+
+    cities = conn.fetchall()
+
+    for city in cities:
+        print(city)
