6e89eeefd595e68a63991506db7cf10a61da0ebc
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-03-17 15:34:26 +0300

    filtering by state

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,17 +5,19 @@ This script takes in an argument and displays all values in the 'states'
 table of 'hbtn_0e_0_usa' where 'name' matches the argument
 """
 
+import MySQLdb
+import sys
+
 if __name__ == '__main__':
 
-    import MySQLdb
-    import sys
-
     args = sys.argv
-    db = MySQLdb.connect(host=args[1], password=args[2], database=args[3])
+
+    db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2], db=args[3])
 
     conn = db.cursor()
 
-    conn.execute("SELECT * FROM states WHERE name=%s", (args[4],))
+    conn.execute("SELECT * FROM states WHERE CONVERT(`name` using Latin1) \
+        COLLATE latin1_General_CS = %s;", (args[4],))
 
     states = conn.fetchall()
 
