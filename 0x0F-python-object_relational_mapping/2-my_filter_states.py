c7847bafe7dc8934d9a143c8c30be74b0a64210c
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-03-17 15:23:23 +0300

    filtering by state

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
+"""
+This script takes in an argument and displays all values in the 'states'
+table of 'hbtn_0e_0_usa' where 'name' matches the argument
+"""
+
 if __name__ == '__main__':
 
     import MySQLdb
     import sys
-
 
     args = sys.argv
     db = MySQLdb.connect(host=args[1], password=args[2], database=args[3])
