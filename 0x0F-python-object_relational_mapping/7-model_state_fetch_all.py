6269c0938015a368fdc704de5015ffe335f70d93
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-03-18 01:41:24 +0300

    solving pycodestyle issues

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,21 +5,18 @@ database 'hbtn_0e_6_usa
 """
 
 import sys
-from inflect import engine
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == '__main__':
-    args = sys.argv
     engine = create_engine(
         'mysql://{}:{}@localhost:3306/{}'
-        .format(args[1], args[2], args[3],))
-
+        .format(sys.argv[1], sys.argv[2], sys.argv[3],))
     Session = sessionmaker(bind=engine)
-    session = Session()
+    Session = Session()
 
-    states = session.query(State) \
-        .order_by(State.id).all()
+    states = Session.query(State).order_by(State.id).all()
+
     for state in states:
         print("{}: {}".format(state.id, state.name))
