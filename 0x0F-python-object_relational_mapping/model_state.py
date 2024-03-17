6611e060033d4cf0d6e31545ec802b3189813893
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-03-17 22:53:03 +0300

    creating a database model using alchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
+
+class State(Base):
+    """State class
+
+    Keyword arguments:
+    Base (class instance) -- this class inherits from the base Class
+    """
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
