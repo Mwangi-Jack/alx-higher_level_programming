2cb0126f897dececf8c04db0235c631abebd3669
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-03-18 09:45:20 +0300

    updating an instance

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+a script that changes the 'name' of a 'State' object from
+the database 'hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import State
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.
+                           format(args[1], args[2], args[3]))
+
+    Session = sessionmaker()
+    session = Session(bind=engine)
+
+    state = session.query(State).filter(State.id == 2).first()
+
+    state.name = 'New Mexico'
+
+    session.commit()
