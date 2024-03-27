8eb51e38379a51e9332d5157e439e76149f56cf8
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-03-27 16:50:50 +0300

    sending POST request with parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sending a POST request with parameters
-curl -sX POST  -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD $1
+curl -sX POST  -d "email=test@gmail.com&subject=I will always be here for PLD" $1
