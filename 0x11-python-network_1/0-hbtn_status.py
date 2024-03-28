ab8fb73fa7edbfff302cd762359ab19992e88f9e
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-03-28 15:37:59 +0300

    fetching a url using python urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import urllib.request
+
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+
+with urllib.request.urlopen(req) as response:
+    body = response.read()
+    print(response.code)
+    # print(response.get('status'))
