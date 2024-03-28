b8422b1b60b09f06a5aa90440f1e416a18fc627c
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-03-28 15:49:35 +0300

    fetching a url using python urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""
+Script to fetch url 'https://alx-intranet.hbtn.io/status'
+"""
 
 import urllib.request
 
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+if __name__ == "__main__":
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 
-with urllib.request.urlopen(req) as response:
-    body = response.read()
-    print(body)
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode('utf-8'))
