39c59701e20afbe583919db8ca4c0197f3da2134
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-17 18:20:04 +0300

    magic string

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Magic string"""
+
+
+def magic_string():
+    """
+    This function returns a string
+    times the number of times it is called
+    """
+
+    if not hasattr(magic_string, 'count'):
+        magic_string.count = 0
+
+    magic_string.count += 1
+
+    return 'BestSchool, ' * (magic_string.count - 1 ) + 'BestSchool'
