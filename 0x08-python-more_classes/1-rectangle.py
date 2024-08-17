b8df18166f304588e0f561ed57527e7892eb26e2
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-17 12:26:08 +0300

    added instance attributes to the Rectangle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,41 +5,35 @@
 
 class Rectangle:
     """This class defines all its methods"""
-
     def __init__(self, width=0, height=0):
         """This method initializes the class"""
-        self.__height = height
-        self.__width = width
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """This method returns the private attribute width"""
+        """This method retrieves the private attribute width"""
         return self.__width
 
     @width.setter
     def width(self, value):
         """This method sets the private attribute width"""
         if not isinstance(value, int):
-            raise TypeError('width must be an integer')
-
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError('width must be >= 0')
-
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """This method returns the private attribute 'height'"""
-
+        """This method retrieves the private attribute height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        "This method sets the height"
-
+        """This method sets the private attribute height"""
         if not isinstance(value, int):
-            raise TypeError('height must be an integer')
-
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError('height must be >= 0')
+            raise ValueError("height must be >= 0")
         self.__height = value
