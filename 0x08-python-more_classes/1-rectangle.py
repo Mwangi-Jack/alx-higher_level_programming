d587cd0c511230cc3bb47ca0aa07b6d2cb3024dc
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-17 12:04:55 +0300

    added instance attributes to the Rectangle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """This method initializes the class"""
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def height(self):
