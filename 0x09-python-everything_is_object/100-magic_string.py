6bff090be96902acd1ac16a08261b7e57dd1d4fc
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-17 18:25:15 +0300

    magic string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-"""Magic string"""
-
-
-def magic_string():
-    """
-    This function returns a string
-    times the number of times it is called
-    """
-
-    if not hasattr(magic_string, 'count'):
-        magic_string.count = 0
-
-    magic_string.count += 1
-
-    return 'BestSchool, ' * (magic_string.count - 1) + 'BestSchool'
+def magic_string(magic=[]):
+    magic += ['BestSchool']
+    return ", ".join(magic)
