c19c349bb4e8922785bb8764e334e6307de7c817
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-17 18:33:51 +0300

    using __slot__ to prevent dynamic creation of new instance attributes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""
+Using __slot__ to prevent dynamic creation
+of instance attributes
+"""
+
 
 class LockedClass:
-    """This class defines """
+    """LockedClass class definition """
     __slots__ = ['first_name']
